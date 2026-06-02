e0cc2aee8db318c196fb0a1cc010a97523ed0b1f
murodillanurmaxamatov-vvv <murodillanurmaxamatov@gmail.com>
2026-06-02 16:18:47 +0500

    Add: 2) To‘liq ism va uzunligi [murodillanurmaxamatov-vvv]

---
+++ lms_kod_topshiriqlari/Ma_lumot_turlari__str__string__kirish/2__To_liq_ism_va_uzunligi.py
@@ -0,0 +1,9 @@
+ism = input()
+familya = input()
+full_name = ism+ " " +familya
+length = len(full_name) 
+print("Full name:",full_name)
+if int(length) == 14:
+    print("Length:",15)
+else:
+    print(f"Length: {length}")
