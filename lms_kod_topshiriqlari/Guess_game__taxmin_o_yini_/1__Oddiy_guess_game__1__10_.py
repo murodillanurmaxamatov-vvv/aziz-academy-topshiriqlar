ca1920fa6b7516838ffd693fc96351a5f52258a4
murodillanurmaxamatov-vvv <murodillanurmaxamatov@gmail.com>
2026-06-04 15:19:51 +0500

    Add: 1) Oddiy guess game (1..10) [murodillanurmaxamatov-vvv]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/1__Oddiy_guess_game__1__10_.py
@@ -0,0 +1,10 @@
+son = 7
+while True:
+    soni = int(input())
+    if soni < son:
+        print("Low")
+    elif soni > son:
+        print("High")
+    else:
+        print("Correct")
+        break
