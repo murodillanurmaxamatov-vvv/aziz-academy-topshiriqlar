cf1d837332d28822d7671a7842c76cf6f1b44800
murodillanurmaxamatov-vvv <murodillanurmaxamatov@gmail.com>
2026-06-04 15:25:29 +0500

    Add: 15) Mini-menyu: append/insert/remove/pop [murodillanurmaxamatov-vvv]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/15__Mini-menyu__append_insert_remove_pop.py
@@ -0,0 +1,28 @@
+lst = []
+while True:
+    cmd = input().split()
+    
+    if cmd[0] == "stop":
+        break
+        
+    elif cmd[0] == "append":
+        lst.append(int(cmd[1]))
+        
+    elif cmd[0] == "insert":
+        i = int(cmd[1])
+        x = int(cmd[2])
+        lst.insert(int(cmd[1]), int(cmd[2]))
+        
+    elif cmd[0] == "remove":
+        x = int(cmd[1])
+        if x in lst:
+            lst.remove(x)
+            
+    
+    elif cmd[0] == "pop":
+        i = int(cmd[1])
+        if 0 <= i < len(lst):
+            lst.pop(i)
+print(lst)            
+        
+        
