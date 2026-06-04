a4e2127be26fabac4bd458b23e0d695dea3fdd42
murodillanurmaxamatov-vvv <murodillanurmaxamatov@gmail.com>
2026-06-04 15:33:00 +0500

    Add: 5) 80 dan yuqori baholar [murodillanurmaxamatov-vvv]

---
+++ lms_kod_topshiriqlari/List_of_dicts__o_quvchilar_va_mahsulotlar/5__80_dan_yuqori_baholar.py
@@ -0,0 +1,7 @@
+n = int(input())
+students = []
+for _ in range(n):
+    name, score = input().split()
+    students.append({'name': name, 'score': int(score)})
+count = sum(1 for s in students if s['score'] > 80)
+print(count)
