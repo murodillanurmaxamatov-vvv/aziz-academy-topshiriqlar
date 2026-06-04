6dd60117d9d5aa796419af15abc51a25945f5a25
murodillanurmaxamatov-vvv <murodillanurmaxamatov@gmail.com>
2026-06-04 15:19:58 +0500

    Add: 3) 1..100 oralig‘ida guess [murodillanurmaxamatov-vvv]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/3__1__100_oralig_ida_guess.py
@@ -0,0 +1,11 @@
+son = 42
+while True:
+    a = int(input())
+    
+    if a < son :
+        print("Low")
+    elif a > son :
+        print("High")
+    else:
+        print("Correct")
+        break
