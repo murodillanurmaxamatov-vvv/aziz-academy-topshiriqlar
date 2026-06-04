49ddbc525c5536bc862c6257d604e7ecf49d14aa
murodillanurmaxamatov-vvv <murodillanurmaxamatov@gmail.com>
2026-06-04 15:19:22 +0500

    Add: 10) Murakkab yoki tub [murodillanurmaxamatov-vvv]

---
+++ lms_kod_topshiriqlari/Oddiy_algoritmlar__sonning_bo_luvchilari__tub_son_/10__Murakkab_yoki_tub.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n <= 1:
+    print("Not Prime")
+else:
+    prime = True
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            prime = False
+            break
+    if prime:
+        print("Prime")
+    else:
+        print("Not Prime")
