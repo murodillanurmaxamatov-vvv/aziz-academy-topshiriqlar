f6a01bde759a53534ddd43b177516974a6e9572a
murodillanurmaxamatov-vvv <murodillanurmaxamatov@gmail.com>
2026-06-09 15:39:08 +0500

    Add: 7) Saralash: Name | Score (score desc, name asc) [murodillanurmaxamatov-vvv]

---
+++ lms_kod_topshiriqlari/Kichik__hisobot__konsolda_chiqarish__table_ko_rini/7__Saralash__Name___Score__score_desc__name_asc_.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+data = []
+
+for _ in range(n):
+    name, score = input().split()
+    score = int(score)
+    data.append((name, score))
+    
+data.sort(key=lambda x: (-x[1], x[0]))
+
+print(f"{'Name':<10} | {'Score':>5}")
+print("-" * 10 + '+' + "-" * 5)
+
+for name, score in data:
+    print(f"{name:<10} | {score:>5}")
