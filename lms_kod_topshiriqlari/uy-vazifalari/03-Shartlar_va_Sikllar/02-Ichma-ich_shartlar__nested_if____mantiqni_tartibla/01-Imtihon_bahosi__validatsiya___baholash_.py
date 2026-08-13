c6c936b5c24ca9d5307da75e0b3f661a9ff3d5dd
murodillanurmaxamatov-vvv <murodillanurmaxamatov@gmail.com>
2026-08-13 15:22:13 +0500

    Add: Imtihon bahosi (validatsiya + baholash) [murodillanurmaxamatov-vvv]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Imtihon_bahosi__validatsiya___baholash_.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a > 100:
+    print("Notogri ball")
+elif a >= 90:
+    print("Alo")
+elif a >= 75:
+    print("Yaxshi")
+elif a >= 60:
+    print("Qoniqarli")
+else:
+    print("Yiqildi")
